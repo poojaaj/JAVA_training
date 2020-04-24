073a50611fb581d16336427f7a2da72527877caa
Pooja Jeergyal <poojaashokjeergyal@gmail.com>
2020-04-24 17:26:46 -0500

    switch example

---
+++ weekOfDay.java
@@ -0,0 +1,32 @@
+
+public class weekOfDay {
+    public static void main(String[] args) {
+        int day = 5;
+        switch(day){
+            case 1:
+            System.out.println("Sunday");
+            break;
+            case 2:
+            System.out.println("Monday");
+            break;
+            case 3:
+            System.out.println("Tuesday");
+            break;
+            case 4:
+            System.out.println("Wednusday");
+            break;
+            case 5:
+            System.out.println("Thursday");
+            break;
+            case 6:
+            System.out.println("Friday");
+            break;
+            case 7:
+            System.out.println("Saturday");
+            break;
+
+        }
+        
+    }
+
+}

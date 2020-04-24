d4399acca9be9316a62e96dc5548775658392b6f
Pooja Jeergyal <poojaashokjeergyal@gmail.com>
2020-04-24 17:20:26 -0500

    eligible to marry

---
+++ eligible.java
@@ -0,0 +1,11 @@
+
+public class eligible {
+    public static void main(String[] args) {
+        int age = 30;
+        if (age >= 18 && age <= 50) {
+            System.out.println("Eligible");
+        } else {
+            System.out.println("not Eligible");
+        }
+    }
+}
